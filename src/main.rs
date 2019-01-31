4d9d8f863c524795e1c636f2f52d744e388f14fc
azu <azu@users.noreply.github.com>
2019-01-31 11:50:34 +0900

    fix(license): fill placeholder in all license (#7)
    * fix(license): fill placeholder in all license
    
    use internal module instead of "license" module
    
    * chore(src): add TODO comment
    
    * chore(src): move statement
    
    * chore(src): Add &

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,8 @@ struct Opt {
     input: String,
     #[structopt(long = "author")]
     author: String,
+    #[structopt(long = "project")]
+    project: Option<String>,
     // TODO: --year support
     // #[structopt(long = "year")]
     // year: u32,
@@ -21,17 +23,25 @@ struct Opt {
 
 fn main() -> Result<(), Box<std::error::Error>> {
     let opt = Opt::from_args();
-    let dt = Local::now();
-    let current_year = dt.year();
     let license = create_license(
-        opt.input.as_str(),
-        opt.author.as_str(),
-        current_year);
+        opt.input.as_str()
+    );
     let license = license.unwrap_or_else(|| {
         eprintln!("Not found match license: {}", opt.input);
         process::exit(1);
     });
-    write_license(&license, "LICENSE").unwrap_or_else(|error| {
+    let dt = Local::now();
+    let current_year = dt.year();
+    let author = opt.author.as_str();
+    // TODO: want to remove clone
+    // TODO: if --project is missing, throw error?
+    let project = opt.project.clone().unwrap_or("The project".to_string());
+
+    let license_text = license.notice(
+        current_year,
+        &author,
+        &project);
+    write_license(&license_text, "LICENSE").unwrap_or_else(|error| {
         eprintln!("Can not write LICENSE file: {}", error);
         process::exit(1);
     });
